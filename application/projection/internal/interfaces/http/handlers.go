85f0466c5f172f5db8aa09ea7886714f7026d4b6
Frederic Gendebien <f.gendebien@napoleongames.be>
2022-02-01 14:07:19 +0100

    Fix projection http interface

--- application/projection/internal/interfaces/http/handlers.go
+++ application/projection/internal/interfaces/http/handlers.go
@@ -4,11 +4,6 @@ import (
 	"github.com/frederic-gendebien/pact-poc/application/projection/internal/domain/model"
 	"github.com/frederic-gendebien/pact-poc/application/projection/internal/usecase"
 	"github.com/gin-gonic/gin"
-	"strconv"
-)
-
-const (
-	MaxLimit int = 20
 )
 
 func addUserHandlers(engine *gin.Engine, useCase usecase.UserProjectionUseCase) {
@@ -18,50 +13,15 @@ func addUserHandlers(engine *gin.Engine, useCase usecase.UserProjectionUseCase) 
 
 func findUsers(useCase usecase.UserProjectionUseCase) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if text := ctx.Query("text"); text != "" {
-			users, err := useCase.FindUsersByText(ctx, text)
-			okOrFail(ctx, err, func() interface{} {
-				return users
-			})
+		text := ctx.Query("text")
+		if text == "" {
+			fail(ctx, model.NewBadRequest("missing mandatory 'text' query param"))
+			return
 		}
 
-		fail(ctx, model.NewBadRequest("missing mandatory 'text' query param"))
-	}
-}
-
-func minOrDefault(value string, maxValue int) int {
-	if value == "" {
-		return maxValue
-	}
-
-	intValue, err := strconv.Atoi(value)
-	if err != nil {
-		return maxValue
-	}
-
-	if intValue > MaxLimit {
-		return MaxLimit
-	}
-
-	return intValue
-}
-
-func accumulateUsers(users <-chan model.User, limit int, next chan<- bool) func() interface{} {
-	return func() interface{} {
-		counter := 0
-		results := make([]model.User, 0, MaxLimit)
-
-		for user := range users {
-			if counter >= limit {
-				next <- false
-				break
-			} else {
-				results = append(results, user)
-				counter++
-				next <- true
-			}
-		}
-
-		return results
+		users, err := useCase.FindUsersByText(ctx, text)
+		okOrFail(ctx, err, func() interface{} {
+			return users
+		})
 	}
 }
